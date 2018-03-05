ebd12ca513362a0f8bd8592ea16846e058f732f8
ArmandoS98 <armandosantosec@gmail.com>
2018-03-05 11:13:03 -0600

    implementacion de: contenedor toolbar

--- app/src/main/java/com/example/user/fragmenttofragmentcomunication/MainActivity.java
+++ app/src/main/java/com/example/user/fragmenttofragmentcomunication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.fragmenttofragmentcomunication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        SelectorFragment fragment = new SelectorFragment();
+        doFragmentTransaction(fragment, getString(R.string.fragment_selector),false,"");
+    }
+
+    private void doFragmentTransaction(Fragment fragment, String tag, boolean addToBackStack, String message){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.main_container,fragment,tag);
+
+        if (addToBackStack){
+            transaction.addToBackStack(tag);
+        }
+        transaction.commit();
     }
 }
